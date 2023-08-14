9d7b59b0e560c40e83fff2b9418763645cc494e6
gabbyb21 <130700719+gabbyb21@users.noreply.github.com>
2023-08-14 10:56:07 -0400

    updated styling

--- client/src/components/ListProjects.js
+++ client/src/components/ListProjects.js
@@ -35,7 +35,51 @@ export default function ListProjects() {
 
   return (
     <>
-    <table className="table mt-5 text-center">
+    <div className="row mt-5">
+      {projects.map(project => (
+        <div className="col-sm-6" key={project.project_id}>
+          <div className="card mb-3">
+            <div className="card-body">
+              <h5 className="card-title">{project.title}</h5>
+              <h6 className="card-subtitle mb-2 text-muted">{project.status}</h6>
+              <p className="card-text">{project.description}</p>
+              <a href="#" className="card-link">Card link</a>
+              <a href="#" className="card-link">Another link</a>
+              <p><EditProject project={project} /></p>
+              <button className="btn btn-danger" onClick={() => deleteProject(project.project_id)}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
+                  <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+
+  {/* <div class="row">
+  <div class="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        {projects.map(project => (
+          <>
+            <h5 className="card-title" key={project.project_id}>{project.title}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">{project.status}</h6>
+            <p className="card-text">{project.description}</p>
+            <a href="#" className="card-link">Card link</a>
+            <a href="#" className="card-link">Another link</a>
+            <p><EditProject project = {project} /></p>
+            <button className="btn btn-danger" onClick={() => deleteProject(project.project_id)}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/></svg>
+              </button> */}
+            </>
+//     ))}
+//       </div>
+//     </div>
+//   </div>
+// </div>
+    /* <table className="table mt-5 text-center">
         <thead>
           <tr>
             <th>Title</th>
@@ -58,7 +102,6 @@ export default function ListProjects() {
             </tr>
           ))}
         </tbody>
-      </table>
-    </>
+      </table> */
   )
 }
