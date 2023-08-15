568a30ef893f00cd98168fe09e644d593127cb22
gabbyb21 <130700719+gabbyb21@users.noreply.github.com>
2023-08-15 15:15:31 -0400

    fixed links bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
-import React,{ Fragment } from 'react';
+import React from 'react';
 import './App.css';
 import InputProject from './components/InputProject';
 import ListProjects from './components/ListProjects';
 
 function App() {
   return (
-   <Fragment>
+   <>
       <div className="container">
         <InputProject />
         <ListProjects />
       </div>
-   </Fragment>
+   </>
   );
 }
 
